3df054bad76385c4117b360b038379cf0aeb98f1
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2023-03-03 09:24:34 +0600

    Add files via upload

---
+++ 1016_distance.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    #Find distance in relative to other car.
+    int distance,time;
+    scanf("%d",&distance);
+    time=distance*2;
+    printf("%d minutos\n",time);
+    return 0;
+}
