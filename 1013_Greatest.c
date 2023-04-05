62a64ea3067c447b662be691cf54ab91867fea1d
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2023-04-05 21:06:49 +0600

    Add files via upload

---
+++ 1013_Greatest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,C,MaiorAB;
+    scanf("%d%d%d",&A,&B,&C);
+    MaiorAB=(A+B+abs(A-B))/2;
+    MaiorAB=(MaiorAB+C+abs(MaiorAB-C))/2;
+    printf("%d eh o maior\n",MaiorAB);
+    return 0;
+}
+
+
