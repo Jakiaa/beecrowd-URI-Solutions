fea4dd39f0bf6d3f78a5cc21177df24009de0deb
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2023-05-11 08:43:45 +0600

    DDD: using switch

---
+++ 1050.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n)
+    {
+    case 11:
+        printf("Sao Paulo\n");
+        break;
+
+    case 19:
+        printf("Campinas\n");
+        break;
+    case 21:
+        printf("Rio de Janeiro\n");
+        break;
+    case 27:
+        printf("Vitoria\n");
+        break;
+    case 31:
+        printf("Belo Horizonte\n");
+        break;
+    case 32:
+        printf("Juiz de Flora\n");
+        break;
+    case 61:
+        printf("Brasilia\n");
+        break;
+    case 71:
+        printf("Salvador\n");
+        break;
+    default:
+        printf("DDD nao cadastrado\n");
+        break;
+
+    }
+    return 0;
+}
